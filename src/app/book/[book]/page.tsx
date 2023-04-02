593887f56fc207ba92adcfeb5ebea8a4bde5ffd8
Giorgi Kobulashvili <gkio@gkios-MacBook-Pro.local>
2023-04-02 16:38:28 +0400

    set if content does not exists

--- src/app/book/[book]/page.tsx
+++ src/app/book/[book]/page.tsx
@@ -3,20 +3,31 @@ import {getBook} from "~/app/utils/get-books";
 import Markdown from 'markdown-to-jsx';
 
 // @ts-ignore
-export default async function Page({ params: { book } }) {
+export default async function Page({params: {book}}) {
   const {name, markdown} = await getBook(book)
 
-  if(!name || !markdown) {
-    return null;
-  }
+  const hasContent = name && markdown
 
   return (
     <div
       className="mx-auto w-full maclassNamexl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
-      <article className="prose lg:prose-xl dark:prose-invert">
-        <h1>{ name }</h1>
-        <Markdown options={{forceWrapper: true, forceBlock:true}}>{markdown}</Markdown>
-      </article>
+      {hasContent && (
+        <article className="prose lg:prose-xl dark:prose-invert">
+          <h1>{name}</h1>
+          <Markdown options={{forceWrapper: true, forceBlock: true}}>{markdown}</Markdown>
+        </article>
+      )}
+
+      {!hasContent && (
+        <article className="prose lg:prose-xl dark:prose-invert">
+          <blockquote>
+            <p>Читаю 🤓</p>
+          </blockquote>
+          <blockquote>
+            <p>Пока заметок нет :(</p>
+          </blockquote>
+        </article>
+      )}
     </div>
   )
 }
